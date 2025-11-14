50a7cc22a86da98bde4effc66fed6397105178d7
Mohammad Ashif <mohammadashif510@gmail.com>
2025-11-15 00:39:40 +0600

    pagination done

--- src/app/utils/queryBuilder.ts
+++ src/app/utils/queryBuilder.ts
@@ -45,4 +45,12 @@ export class QueryBuilder<T> {
         this.modelQuery = this.modelQuery.select(fields);
         return this;
     }
+
+    paginate(): this {
+        const page = Number(this.query.page) || 1;
+        const limit = Number(this.query.limit) || 10;
+        const skip = (page - 1) * limit;
+        this.modelQuery = this.modelQuery.skip(skip).limit(limit);
+        return this;
+    }
 }
